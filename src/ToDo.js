83d986078ab000b790d10067fedf62ec26d693fd
w-hosinski <woj.hosinski@gmail.com>
2021-12-21 19:54:28 +0100

    Erledigte Items mit Click markieren funktion
    - erledigte Items können mit einem Klick auf den Text als erledigt markiert werden
    - checkboxen entfernt
    - "remove all completed" button hinzugefügt

--- src/ToDo.js
+++ src/ToDo.js
@@ -4,29 +4,42 @@ const ToDo = () => {
   const [tasks, setTasks] = useState([])
   const [input, setInput] = useState("")
 
-  const removeItem = (id) => {
+  const removeItem = id => {
     let newTasks = tasks.filter((task) => task.id !== id);
     setTasks(newTasks)
   }
 
   const updateTasks = () => {
-    setTasks([...tasks, {id: input, name: input}])
+    setTasks([...tasks, {id: input, name: input, completed: false}])
     setInput("")
+  }
+
+  const handleCompleted = id => {
+    console.log("TEST")
+    let mapped = tasks.map((task)=>{
+      return task.id === id ? {...task, completed: !task.completed} : { ...task}})
+    setTasks(mapped)
+  }
+
+  const removeCompleted = () => {
+    let newTasks = tasks.filter((task) => task.completed === false);
+    setTasks(newTasks)
   }
 
   return (
     <div className="boundingBox">
       <input className="inputField" value={input} onChange={(e)=>setInput(e.target.value)}></input>
       <button className="addTaskBtn" onClick={updateTasks}>Add Task</button>
-      <button className='btn' onClick={() => setTasks([])}>Clear All Tasks</button>
+      <button className='btn' onClick={removeCompleted}>Clear All Completed</button>
+      <button className='btn' onClick={() => setTasks([])}>Clear ALL</button>
+      
       <div className="listTask">
         {tasks.map((task) => {
           const { id, name } = task
           return (
-            <div key={id} className='task'>
-              <h4 className="name">{name}</h4>
-              <input type="checkbox"></input>
-              <button onClick={() => removeItem(id)}>X</button>
+            <div key={id} className="task">
+              <h4 type="button" onClick={()=>handleCompleted(id)} className={task.completed ? "strike" : ""}>{name}</h4>
+              <button onClick={() => removeItem(id)}>Delete</button>
             </div>
           )
         })}
